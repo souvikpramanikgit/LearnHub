77496b039e04a0d5f726ab930044dedeba49356b
souvikpramanikgit <souvikpramanik874@gmail.com>
2025-07-03 01:36:03 +0530

    bug fixes

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({}) => ({
   server: {
     host: "::",
     port: 8080,
