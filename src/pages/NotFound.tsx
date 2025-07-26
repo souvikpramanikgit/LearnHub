5ed746b13e03ba8e2b7e2c43bbd5bf564d82d233
Isha Goyal <goyalisha261@gmail.com>
2025-07-26 15:06:14 +0530

    ✨ Added responsive custom 404 page with dark mode toggle and navigation buttons

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,6 +1,7 @@
 import { useLocation } from "react-router-dom";
+import Lottie from "lottie-react";
+import NotFoundAnimation from "../assets/pet.json"
 import { useEffect } from "react";
-
 const NotFound = () => {
   const location = useLocation();
 
@@ -12,13 +13,17 @@ const NotFound = () => {
   }, [location.pathname]);
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    // <div className="min-h-screen flex items-center justify-center bg-gray-100">
+     <div className="min-h-screen flex flex-col items-center justify-center text-center px-4 md:px-8  bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-gray-100">
       <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4">404</h1>
-        <p className="text-xl text-gray-600 mb-4">Oops! Page not found</p>
-        <a href="/" className="text-blue-500 hover:text-blue-700 underline">
-          Return to Home
-        </a>
+        <Lottie animationData={NotFoundAnimation} loop={true} />
+        <h1 className="text-8xl font-extrabold text-blue-600">404</h1>
+        <p className="text-xl mt-4 text-gray-700 dark:text-gray-300">Sorry, page not found.</p>
+        <div className="flex justify-center gap-4 mt-6 flex-wrap mt-4 inline-block px-6 py-3 bg-blue-600 text-white font-semibold rounded-xl hover:bg-blue-700 dark:bg-blue-500 dark:hover:bg-blue-600 transition">
+          <a href="/" className="btn">🏠 Home</a>
+          <a href="/course/intro-to-react" className="btn">📘 Courses</a>
+          <a href="/dsa-sheet" className="btn">🧩 DSA Sheet</a>
+        </div>
       </div>
     </div>
   );
