22f538363bd5c5e6386b1b137a49f23094415273
souvikpramanikgit <souvikpramanik874@gmail.com>
2025-08-12 19:16:58 +0530

    Add changes

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -81,15 +81,6 @@ const config: Config = {
       },
       items: [
         {
-          to: '/#courses-section',
-          label: 'Courses',
-          position: 'left',
-          className: 'navbar__link',
-          activeBaseRegex: '^$', // prevents it from being marked as active
-          target: '_self',
-        },
-
-        {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
